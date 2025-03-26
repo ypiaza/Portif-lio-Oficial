195889d1843fcdea1854de7d00c2a700cbe10fc2
Yuri Piaza <yuripiazagomessantos@gmail.com>
2025-03-26 13:36:17 -0300

    Adicionado icones das skills e inciado o processo de animação

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,7 +1,19 @@
-const Skills = () => {
-  return (
-    <div className='px-12 py-14 rounded-3xl border border-white/10 bg-[#202020]'>
+import { useState } from "react"
+import { itemSkill, skills } from "../data/skills"
 
+
+const Skills = () => {
+  const [hoverIcon, setHoverIcon] = useState(false)
+  return (
+    <div className='grid grid-cols-4 overflow-hidden rounded-3xl border text-white border-white/10 bg-[#202020]'>
+      {skills.map((item: itemSkill) => (
+        <div className={`relative text-4xl border border-white/10 flex items-center justify-center overflow-hidden`}
+        onMouseOver={() => setHoverIcon(true)}
+        onMouseOut={() => setHoverIcon(false)}>
+          <item.icon />
+          <span className={`absolute block h-full w-0 bg-amber-200/30 left-0 duration-300 ${hoverIcon ? 'w-full' : ''}`}></span>
+        </div>
+      ))}
     </div>
   )
 }
--- src/data/skills.ts
+++ src/data/skills.ts
@@ -1,8 +1,9 @@
 export interface itemSkill{
     skill: string,
-    icon : Element;
+    icon : React.ElementType
 }
 
+import React from "react";
 import { FaReact, FaGithub, FaHtml5, FaCss3 } from "react-icons/fa";
 import { RiTailwindCssFill, RiNextjsFill } from "react-icons/ri";
 import { SiTypescript, SiJavascript } from "react-icons/si";
@@ -10,6 +11,27 @@ import { SiTypescript, SiJavascript } from "react-icons/si";
 export const skills: itemSkill[] = [
     {
         skill: 'React',
-        icon: <FaReact />;
-    }
+        icon: FaReact
+    },{
+        skill: 'React',
+        icon: FaGithub
+    },{
+        skill: 'React',
+        icon: FaHtml5
+    },{
+        skill: 'React',
+        icon: FaCss3
+    },{
+        skill: 'React',
+        icon: RiTailwindCssFill
+    },{
+        skill: 'React',
+        icon: RiNextjsFill
+    },{
+        skill: 'React',
+        icon: SiTypescript
+    },{
+        skill: 'React',
+        icon: SiJavascript
+    },
 ]
