bd8642a076249322495d2a1111b0a84d382f06ee
Yuri Piaza <yuripiazagomessantos@gmail.com>
2025-04-01 13:38:59 -0300

    Update

--- src/components/Apresentation.tsx
+++ src/components/Apresentation.tsx
@@ -2,9 +2,10 @@ import { btn } from "../data/buttons"
 import AvaliableForJob from "./AvaliableForJob"
 
 const Apresentation = () => {
+
     return (
-        <div className=" grid grid-cols-1 lg:grid-cols-2 items-center justify-between h-full lg:h-[25rem] px-12 py-14 rounded-3xl border border-white/10 bg-[#202020] mt-3">
-            <div className="h-full flex flex-col items-start justify-between">
+        <div className="flex w-full lg:grid lg:grid-cols-2 items-center justify-between h-full lg:h-[25rem] px-12 py-14 rounded-3xl border border-white/10 bg-[#202020] mt-3">
+            <div className="h-full w-full flex flex-col items-start justify-between">
                 <div>
                     <h1 className="text-white/70">Desenvolvedor Frontend</h1>
                 </div>
@@ -13,14 +14,25 @@ const Apresentation = () => {
                         <h2 className="text-2xl font-semibold text-white">Eu sou Yuri Piaza</h2>
                         <p className="text-sm text-white/70 mt-5">Crio interfaces bonitas e escaláveis, desenvolvedor frontend com foco em desenvolvimento web</p>
                     </div>
-                    <div className="flex items-center gap-4 mt-8">
+                    <div className="flex flex-col lg:flex-row items-center gap-4 mt-8">
                         {btn.map((item, index) => (
-                            <button key={index} className={`border border-white/10 px-2 py-1.5 rounded-md text-white font-medium cursor-pointer`}
-                                style={{
-                                    backgroundColor: item.color,
-                                    boxShadow: `0 0 10px 5px ${item.color}70`
-                                }}
-                            >{item.name}</button>
+                            <div key={index} className=" cursor-pointer flex items-center justify-center border-2 rounded overflow-hidden border-white/10">
+                                <button className={`px-2 py-0.75 text-white font-medium cursor-pointer`}
+                                    style={{
+                                        backgroundColor: item.color,
+                                        boxShadow: `0 0 10px 5px ${item.color}70`
+                                    }}
+                                >{item.name}</button>
+                                <div>
+                                    <item.icon className='text-3xl border-l-2 border-white/10 text-white px-1'
+                                        style={{
+                                            backgroundColor: item.color,
+                                            boxShadow: `0 0 10px 5px ${item.color}70`
+                                        }} />
+                                </div>
+
+                            </div>
+
                         ))}
                     </div>
                 </div>
@@ -30,7 +42,7 @@ const Apresentation = () => {
                 <div className="flex flex-col items-center justify-between h-full">
                     <AvaliableForJob />
                     <div>
-                        <img  className="rounded-full border-8 border-white/10" src='https://placehold.co/200' alt="Me" />
+                        <img className="rounded-full border-8 border-white/10" src='https://placehold.co/200' alt="Me" />
                     </div>
                 </div>
             </div>
--- src/data/buttons.ts
+++ src/data/buttons.ts
@@ -1,17 +1,25 @@
+import React from "react"
+import { MdAdd } from "react-icons/md";
+import { IoCopy } from "react-icons/io5";
+
+
 export interface itemBtn {
     link: string,
     name: string,
-    color: string
+    color: string,
+    icon: React.ElementType
 }
 
 export const btn: itemBtn[] = [
     {
-        link: '/',
-        name: 'Me contrate',
-        color: '#4D66FF'
+        link: 'https://wa.me/5579998671726',
+        name: 'Fale comigo',
+        color: '#4D66FF',
+        icon: MdAdd
     },{
         link: '/',
         name: 'Meu email',
-        color: '#161616'
+        color: '#161616',
+        icon: IoCopy
     }
 ]
